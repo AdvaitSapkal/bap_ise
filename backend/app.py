1c3b4da9cc5b9c7340364ff352b6d8302fe412f4
AdvaitSapkal <115008586+AdvaitSapkal@users.noreply.github.com>
2024-04-29 18:02:20 +0530

    initial_commit

--- backend/app.py
+++ backend/app.py
@@ -3,14 +3,11 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load the CSV file into a DataFrame
 df = pd.read_csv('./bap_ise_data.csv')
 
 @app.route('/query/<column>/<value>')
 def query_csv(column, value):
-    # Filter DataFrame based on column and value
     filtered_df = df[df[column] == value]
-    # Convert filtered DataFrame to JSON
     result = filtered_df.to_dict(orient='records')
     return jsonify(result)
 
