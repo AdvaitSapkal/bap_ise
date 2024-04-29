475accd004b91d8e100682642f4ba8f8f8236c8f
AdvaitSapkal <115008586+AdvaitSapkal@users.noreply.github.com>
2024-04-29 18:23:27 +0530

    added room availability

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,30 @@ app = Flask(__name__)
 
 df = pd.read_csv('./bap_ise_data.csv')
 
+total_rooms=set(df['room_num'].unique())
+
+
+
+#individual tt of faculty
 @app.route('/query/<column>/<value>')
 def query_csv(column, value):
     filtered_df = df[df[column] == value]
     result = filtered_df.to_dict(orient='records')
     return jsonify(result)
+
+#room availability
+@app.route('/query/availability/<start>/<end>/<day>')
+def query_rooms(start, end, day):
+    start_time = pd.to_datetime(start, format='%H:%M').time()
+    end_time = pd.to_datetime(end, format='%H:%M').time()
+
+    filtered_df = df[(df['day'] == day) & ((df['start_time'] == start_time) | (df['end_time'] == end_time))]
+
+    room_numbers = set(filtered_df['room_num'])
+
+    result_set=total_rooms-room_numbers
+
+    return jsonify(list(result_set))
 
 if __name__ == '__main__':
     app.run(debug=True)
